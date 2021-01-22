785ff489723a01a355c1d7e0d9e7b895174e62c8
GuOffL <jandersongusta@gmail.com>
2021-01-21 22:18:02 -0300

    ...
    ...

--- src/view/comandos.java
+++ src/view/comandos.java
@@ -6,10 +6,10 @@ public class comandos {
     public static void Iniciar() {
         API.RequisitarTODAS_AS_MOEDAS();
         escreverMenu();
-        realpDolar();
-        realpEuro();
-        realpLibra();
-        realpPeso();
+        real_p_Dolar();
+        real_p_Euro();
+        real_p_Libra();
+        real_p_Peso();
     }
     
     public static void escreverMenu(){
@@ -19,22 +19,22 @@ public class comandos {
     
     
     
-    public static double realpDolar(){
+    public static double real_p_Dolar(){
         double real = 1, cotD;
         cotD = real * bdMoedas.DOLAR.getCotacao();
         return cotD;
     }
-    public static double realpEuro(){
+    public static double real_p_Euro(){
         double real = 1, cotE;
         cotE = real * bdMoedas.EURO.getCotacao();
         return cotE;
     }
-    public static double realpLibra(){
+    public static double real_p_Libra(){
         double real = 1, cotL;
         cotL = real * bdMoedas.LIBRA.getCotacao();
         return cotL;
     }
-    public static double realpPeso(){
+    public static double real_p_Peso(){
         double real = 1, cotP;
         cotP = real * bdMoedas.PESO.getCotacao();
         return cotP;
