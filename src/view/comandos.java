8abef818334535bf8501b4e5fd0784e2ae2065cb
GuOffL <jandersongusta@gmail.com>
2021-01-21 22:02:24 -0300

    alteração 1
    ado ado

--- src/view/comandos.java
+++ src/view/comandos.java
@@ -11,4 +11,28 @@ public class comandos {
     public static void escreverMenu(){
         
     }
+    
+    
+    
+    
+    public static double realpDolar(){
+        double real = 1, cotD;
+        cotD = real * bdMoedas.DOLAR.getCotacao();
+        return cotD;
+    }
+    public static double realpEuro(){
+        double real = 1, cotE;
+        cotE = real * bdMoedas.EURO.getCotacao();
+        return cotE;
+    }
+    public static double realpLibra(){
+        double real = 1, cotL;
+        cotL = real * bdMoedas.LIBRA.getCotacao();
+        return cotL;
+    }
+    public static double realpPeso(){
+        double real = 1, cotP;
+        cotP = real * bdMoedas.PESO.getCotacao();
+        return cotP;
+    }
 }
